84ae51099ce223b7a518f3105fbd43feece02547
PaulErny <paul.erny@epitech.eu>
2024-06-10 21:45:25 +0200

    FIX - crash when unselecting items

--- Withings/Withings/Views/ViewController.swift
+++ Withings/Withings/Views/ViewController.swift
@@ -98,6 +98,7 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         (collectionView.cellForItem(at: indexPath) as! ImageCollectionViewCell).checkmark.isHidden = true
-        selectedImages.remove(at: indexPath.item)
+        selectedImages.removeAll(where: { $0.id == images[indexPath.item].id })
+//        selectedImages.remove(at: indexPath.item)
     }
 }
